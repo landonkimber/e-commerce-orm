6eaf03f8a76d85347f79f294714c3dac2eee4830
landonkimber <landonkimber33@gmail.com>
2024-02-03 14:00:26 -0700

    added all files from the challenge develop folder. Completed defining columns for all models. Started developing html functions in routes/.

---
+++ routes/api/category-routes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const data = await Category.findAll();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// url/api/categories/
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const locationData = await Category.create(req.body);
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
